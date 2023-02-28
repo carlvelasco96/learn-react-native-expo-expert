158dca36da98ab075227e49da35da002229b3600
Carl Velasco <carlvelasco96@gmail.com>
2023-03-01 10:57:35 +1300

    Improved design of Input component

--- components/UI/Input.tsx
+++ components/UI/Input.tsx
@@ -4,8 +4,11 @@ import {
   TextInput,
   View,
   TextInputProps,
+  StyleProp,
+  TextStyle,
 } from "react-native";
 import React from "react";
+import { GlobalStyles } from "../../constants/styles";
 
 type Props = {
   label: string;
@@ -13,14 +16,41 @@ type Props = {
 };
 
 const Input = ({ label, textInputConfig }: Props) => {
+  let inputStyles: StyleProp<TextStyle>[] = [styles.input];
+
+  if (textInputConfig?.multiline) {
+    inputStyles.push(styles.inputMultiline);
+  }
+
   return (
-    <View>
-      <Text>{label}</Text>
-      <TextInput {...textInputConfig} />
+    <View style={styles.inputContainer}>
+      <Text style={styles.label}>{label}</Text>
+      <TextInput style={inputStyles} {...textInputConfig} />
     </View>
   );
 };
 
 export default Input;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  inputContainer: {
+    marginHorizontal: 4,
+    marginVertical: 8,
+  },
+  label: {
+    fontSize: 12,
+    color: GlobalStyles.colors.primary100,
+    marginBottom: 4,
+  },
+  input: {
+    backgroundColor: GlobalStyles.colors.primary100,
+    color: GlobalStyles.colors.primary700,
+    padding: 6,
+    borderRadius: 6,
+    fontSize: 18,
+  },
+  inputMultiline: {
+    minHeight: 100,
+    textAlignVertical: "top",
+  },
+});
